c8459e6b4d68b6b5920f92e6dfd3f1a3ec51def0
Chrysanthe Belgique <belgiquc@byu.edu>
2024-01-17 09:55:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Mission1Assignment;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Initialize RollDice class
+        RollDice roll = new RollDice();
+
+        // Initialize variable
+        int numRolls = 0;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        // Test numRolls
+        //System.Console.WriteLine(numRolls);
+
+        // Call RollDice class
+        int[] finalArray = roll.rollIt(numRolls);
+
+        // Display final message
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS \n" + 
+                                 "Each '*' represents 1 % of the total number of rolls.\n" +
+                                 "Total number of rolls = " + numRolls + "\n");
+
+        // Display the final array
+        for (int i = 0; i < finalArray.Length; i++)
+        {
+            // Calculate the percentage (rounded) of occurences
+            int percentage = (int)Math.Round((finalArray[i]  * 100.0/ numRolls));
+
+            // Print the total num :
+            System.Console.Write((i + 2) + ": ");
+
+            // Print out one * for each percent
+            for (int j = 0; j < percentage; j++)
+            {
+                System.Console.Write("*");
+            }
+
+            // Print a new line
+            System.Console.WriteLine();
+
+        }
+
+        // Display goodbye message
+        System.Console.Write("\nThank you for using the dice throwing simulator. Goodbye!");
+
+
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission1Assignment
+{
+    internal class RollDice
+    {
+        public int[] rollIt(int numRolls)
+        {
+            // Create a Random object
+            Random random = new Random();
+
+            // Create array to keep track of number of times rolled each combo (2-12)
+            int[] rollsArray = new int[11];
+
+            // Go through both dice the number of rolls the user specifies
+            for (int i = 0; i < numRolls; i++)
+            {
+                int roll1 = random.Next(1, 7);
+                int roll2 = random.Next(1, 7);
+
+                int total = roll1 + roll2;
+
+                rollsArray[total - 2] += 1;
+            }
+
+            // Return the array
+            return rollsArray;
+        }
+    }
+}
